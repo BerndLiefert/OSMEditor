bcde8f2a1ee9cca64d8ae886cbc2d35e9bb7cf8e
Bernd Liefert <vaiobernd@gmai.com>
2020-04-15 14:23:10 +0200

    removed index-new.js

--- src/index.ts
+++ src/index.ts
@@ -1,124 +1,114 @@
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import { Draw, Modify, Snap } from 'ol/interaction';
-import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import { Tile as TileLayer } from 'ol/layer';
 import { OSM, Vector as VectorSource } from 'ol/source';
-import { Fill, Icon, Stroke, Style } from 'ol/style';
-import Select from 'ol/interaction/Select';
-import Styles from './lib/styles.js';
 import Loader from './lib/loader.js';
-import Editor from './lib/editor.js';
-import GeometryType from 'ol/geom/GeometryType';
-let color = '#FF11A3';
-var raster = new TileLayer({
-  source: new OSM()
-});
+import Styles from './lib/styles.js';
+import VectorLayer from 'ol/layer/Vector';
+import { Style, Fill, Stroke, Icon } from 'ol/style';
+import { Modify, Draw, Snap, Select } from 'ol/interaction';
 
-var styles = new Styles(color);
-var styleFunction = styles.style.bind(styles);
+const color = '#FF11A3';
+class EditorPlugin {
+  protected draw: Draw;
+  protected snap: Snap;
+  protected modify: Modify;
+  protected select: Select;
+  protected map: Map
+  protected source: VectorSource;
 
-var source = new VectorSource();
+  constructor() {
+    let raster = new TileLayer({
+      source: new OSM()
+    });
+    this.map = new Map({
+      layers: [raster],
+      target: 'map',
+      view: new View({
+        center: [-11000000, 4600000],
+        zoom: 4
+      })
+    });
+    let loader = new Loader('./berlin_bezirke.geojson');
+    let berlin = loader.getVectorLayer();
 
-var arrow = new VectorLayer({
-  source: source,
-  style: styleFunction
-});
+    this.map.addLayer(berlin);
+    var styles = new Styles(color);
+    var styleFunction = styles.style.bind(styles);
 
-var vector = new VectorLayer({
-  source: source,
-  style: new Style({
-    fill: new Fill({
-      color: 'rgba(255, 255, 255, 0.2)'
-    }),
-    stroke: new Stroke({
-      color: color,
-      width: 2
-    }),
-    image: new Icon({
-      src: './assets/marker.png',
-      size: [128, 128],
-      offset: [0, 0],
-      opacity: 1,
-      scale: 0.25
-    })
-  })
-});
+    this.source = new VectorSource();
 
-let loader = new Loader('./berlin_bezirke.geojson');
-let berlin = loader.getVectorLayer();
+    var arrow = new VectorLayer({
+      source: this.source,
+      style: styleFunction
+    });
 
+    var vector = new VectorLayer({
+      source: this.source,
+      style: new Style({
+        fill: new Fill({
+          color: 'rgba(255, 255, 255, 0.2)'
+        }),
+        stroke: new Stroke({
+          color: color,
+          width: 2
+        }),
+        image: new Icon({
+          src: './assets/marker.png',
+          size: [128, 128],
+          offset: [0, 0],
+          opacity: 1,
+          scale: 0.25
+        })
+      })
+    });
 
+    this.map.addLayer(arrow);
+    this.map.addLayer(vector);
 
+    this.modify = new Modify({ source: this.source });
+    this.map.addInteraction(this.modify);
 
+    let buttons = document.getElementsByName('tool');
 
+    for (let button of buttons) {
+      button.addEventListener('click', (e) => {
+        this.map.removeInteraction(this.draw);
+        this.map.removeInteraction(this.snap);
+        this.addInteractions();
+      })
+    }
 
+    this.addInteractions();
 
+    let labels = document.querySelectorAll('label');
 
-
-var map = new Map({
-  layers: [raster, vector, arrow, berlin],
-  target: 'map',
-  view: new View({
-    center: [-11000000, 4600000],
-    zoom: 4
-  })
-});
-
-
-
-
-var modify = new Modify({ source: source });
-map.addInteraction(modify);
-
-
-var draw, snap, select; // global so we can remove them later
-
-function addInteractions() {
-  draw = new Draw({
-    source: source,
-    type: document.querySelector('input[name="tool"]:checked').value
-  });
-  map.addInteraction(draw);
-  snap = new Snap({ source: source });
-  map.addInteraction(snap);
-  /*
-  select = new Select();
-  map.addInteraction(select);
-  */
-
-}
-
-/**
- * Handle change event.
- */
-let buttons = document.getElementsByName('tool');
-
-for (let button of buttons) {
-  button.addEventListener('click', function (e) {
-    map.removeInteraction(draw);
-    map.removeInteraction(snap);
-    //map.removeInteraction(select);
-    addInteractions();
-  })
-}
-
-addInteractions();
-
-let labels = document.querySelectorAll('label');
-
-for (let label of labels) {
-  label.addEventListener('click', function () {
-    unstyle();
-    this.style.setProperty('opacity', '1');
-  });
-}
-
-function unstyle() {
-
-  let labels = document.querySelectorAll('label');
-
-  for (let label of labels) {
-    label.style.setProperty('opacity', '.3');
+    for (let label of labels) {
+      label.addEventListener('click', function() {
+        plugin.unstyle();
+        this.style.setProperty('opacity', '1');
+      });
+    }
   }
+
+  addInteractions() {
+    this.draw = new Draw({
+      source: this.source,
+      type: document.querySelector('input[name="tool"]:checked').value
+    });
+    this.map.addInteraction(this.draw);
+    this.snap = new Snap({ source: this.source });
+    this.map.addInteraction(this.snap);
+  }
+
+  unstyle() {
+    let labels = document.querySelectorAll('label');
+
+    for (let label of labels) {
+      label.style.setProperty('opacity', '.3');
+    }
+  }
+
 }
+let plugin = new EditorPlugin();
